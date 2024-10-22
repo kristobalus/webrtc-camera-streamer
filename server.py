269e6adc74bc57b69e4b5576edde348f3b6598f7
kristobalus <ildar.tamendarov@gmail.com>
2024-10-22 19:14:56 +0500

    update

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from av import VideoFrame
 import fractions
 from datetime import datetime
 from aiohttp_cors import ResourceOptions, setup
+import numpy as np
 
 
 class CustomVideoStreamTrack(VideoStreamTrack):
@@ -14,26 +15,48 @@ class CustomVideoStreamTrack(VideoStreamTrack):
         super().__init__()
         self.cap = cv2.VideoCapture(camera_id)
         self.frame_count = 0
+        self.format = "rgb24"
+        self.frame_rate = 100
+
+        w = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        h = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        print('resolution is: ', w, 'x', h)
+
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+        self.cap.set(cv2.CAP_PROP_FPS, self.frame_rate)
+
+        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 0.5)
+        self.cap.set(cv2.CAP_PROP_CONTRAST, 0.5)
+        self.cap.set(cv2.CAP_PROP_SATURATION, 0.5)
+        self.cap.set(cv2.CAP_PROP_AUTOFOCUS, 1)
 
     async def recv(self):
         self.frame_count += 1
-        print(f"Sending frame {self.frame_count}")
+        # print(f"Sending frame {self.frame_count}")
         ret, frame = self.cap.read()
         if not ret:
             print("Failed to read frame from camera")
             return None
+
+        # Improve image quality by denoising
+        # frame = cv2.fastNlMeansDenoisingColored(frame, None, 10, 10, 7, 21)
+        # Sharpen the image
+        # kernel = np.array([[0, -1, 0],
+        #                    [-1, 5, -1],
+        #                    [0, -1, 0]])
+        # frame = cv2.filter2D(src=frame, ddepth=-1, kernel=kernel)
+
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        video_frame = VideoFrame.from_ndarray(frame, format="rgb24")
-        video_frame.pts = self.frame_count
-        video_frame.time_base = fractions.Fraction(1, 30)  # Use fractions for time_base
+
         # Add timestamp to the frame
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]  # Current time with milliseconds
         cv2.putText(frame, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        video_frame = VideoFrame.from_ndarray(frame, format="rgb24")
+        video_frame = VideoFrame.from_ndarray(frame, format=self.format)
         video_frame.pts = self.frame_count
-        video_frame.time_base = fractions.Fraction(1, 30)  # Use fractions for time_base
+        video_frame.time_base = fractions.Fraction(1, self.frame_rate)  # Use fractions for time_base
+
         return video_frame
 
 
